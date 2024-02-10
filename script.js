25db92dcb731ae8e917ef57a3ea78b33b30b18ef
Alexander Sharif <119440730+Alexanua@users.noreply.github.com>
2024-02-10 14:36:53 +0100

    Update script.js
    fixa an error

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let x = 5;
-let y = 7;
+let x = 5
 
-function double(number) {
-  return number * 2;
+function double(num) {
+  return num * 2
 }
 
-console.log("Dubbelt av 5 är " + double(x));
+console.log('Dubbelt av 5 är ' + double(x))
+
