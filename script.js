1e11d6ec7100389cf01e20af5a1447cfa39e0e0e
Alexander Sharif <119440730+Alexanua@users.noreply.github.com>
2024-02-10 14:27:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let x = 5
-  let y = 7
+let x = 5;
+let y = 7;
 
-function double(x) { return x*2 )
+function double(z) {
+  return z * 2;
+}
 
-console.print(“Dubbelt av 5 är “ + double(x));
+console.log("Dubbelt av 5 är " + double(x));
